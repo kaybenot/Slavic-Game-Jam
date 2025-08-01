8a46bdc8bd2b178ff4661945fed55a3530951f81
Kamil Butryn <kaybenot@gmail.com>
2025-08-01 20:35:47 +0200

    Test input

---
+++ Assets/Scripts/System/Server/TestInputProcessingSystem.cs
@@ -0,0 +1,37 @@
+﻿using Data.Player;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.NetCode;
+using UnityEngine;
+
+namespace Systems.Network.Server
+{
+    [WorldSystemFilter(WorldSystemFilterFlags.ServerSimulation)]
+    public partial struct TestInputProcessingSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<PlayerInputData>();
+            state.RequireForUpdate<NetworkTime>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            //var networkTime = SystemAPI.GetSingleton<NetworkTime>();
+            
+            foreach (var (playerInputData, entity) in SystemAPI.Query<RefRO<PlayerInputData>>()
+                         .WithEntityAccess().WithAll<Simulate>())
+            {
+                //if (networkTime.IsFirstTimeFullyPredictingTick)
+                //{
+                    if (playerInputData.ValueRO.TestAction.IsSet)
+                    {
+                        Debug.Log("Test action triggered on server for entity: " + entity.Index);
+                    }
+                //}
+            }
+        }
+    }
+}
