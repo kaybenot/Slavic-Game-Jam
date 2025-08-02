ea8e805280c05b061efe0cd19dd476e593dc4261
Kosmik <duvin.kosmik@gmail.com>
2025-08-02 22:54:53 +0200

    Add world helper

---
+++ Assets/Scripts/Helpers/Network/Rpc/WorldHelper.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+
+namespace Helpers.Network
+{
+	public static class WorldHelper
+    {
+        public static World GetClientWorld()
+		{
+			for (int w = 0; w < World.All.Count; w++)
+				if (World.All[w].Name == "ClientWorld")
+					return World.All[w];
+
+			return default;
+		}
+    }
+}
