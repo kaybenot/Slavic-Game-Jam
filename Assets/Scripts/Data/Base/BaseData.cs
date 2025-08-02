bfecaf8c686793b1f907151d7c6a2fca2c805180
Kamil Butryn <kaybenot@gmail.com>
2025-08-02 23:20:11 +0200

    kruawyuroawuhrpawohufop;aj krurwaaa

--- Assets/Scripts/Data/Base/BaseData.cs
+++ Assets/Scripts/Data/Base/BaseData.cs
@@ -1,11 +1,13 @@
 ﻿using Helpers.Base;
 using Unity.Entities;
+using Unity.NetCode;
 
 namespace Data.Base
 {
     public struct BaseData : IComponentData
     {
         public BaseType BaseType;
+        [GhostField] public int Health;
     }
 }
 
