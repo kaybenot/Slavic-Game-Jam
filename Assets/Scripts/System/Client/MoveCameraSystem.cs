3b56b316fb2614d712b3f7b7123f8917f7b18dbc
Kamil Butryn <kaybenot@gmail.com>
2025-08-03 12:38:29 +0200

    Camera movement

---
+++ Assets/Scripts/System/Client/MoveCameraSystem.cs
@@ -0,0 +1,31 @@
+﻿using Data.Camera;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace System.Client
+{
+    [WorldSystemFilter(WorldSystemFilterFlags.ClientSimulation)]
+    public partial struct MoveCameraSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<CameraTargetData>();
+        }
+        
+        public void OnUpdate(ref SystemState state)
+        {
+            foreach (var localTransform
+                     in SystemAPI.Query<RefRW<LocalTransform>>().WithAll<CameraTargetData>())
+            {
+                var x = Input.GetAxisRaw("Horizontal");
+                var y = Input.GetAxisRaw("Vertical");
+
+                localTransform.ValueRW.Position += new float3(x, 0f, y);
+            }
+        }
+    }
+}
